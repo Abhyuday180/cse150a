c87cf87a87196cbf35c6f078ab5cfe5db8d27ebb
j0345 <64547030+j0345@users.noreply.github.com>
2025-03-16 22:45:58 -0700

    Update bayesian_network.py based on MLE training

--- src/bayesian_network.py
+++ src/bayesian_network.py
@@ -1,10 +1,20 @@
 from pgmpy.models import BayesianNetwork
+from pgmpy.estimators import MaximumLikelihoodEstimator
 
 def build_network():
-    model = BayesianNetwork([
-        ('RSI_Cat', 'Price_Movement'),
-        ('Vol_Cat', 'Price_Movement'),
-        ('Vol_Chg', 'Price_Movement'),
-        ('Price_Movement', 'Action')
+    model = BayesianModel([
+        ('MarketTrend', 'PriceMovement'),
+        ('RSI_Level', 'PriceMovement'),
+        ('TradingVolume', 'VolatilityLevel'),
+        ('VolatilityLevel', 'PriceMovement')
     ])
+    
+    # Fit the model using Maximum Likelihood Estimator from pgmpy
+    model.fit(data, estimator=MaximumLikelihoodEstimator)
+    
+    # Print out the learned CPDs (Conditional Probability Distributions)
+    print("Learned CPDs:")
+    for cpd in model.get_cpds():
+        print(cpd)
+    
     return model
